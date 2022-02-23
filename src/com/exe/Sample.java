3bd1289f171f0a47a3fac85dc38187f083b80cf7
vikram4026 <gmvikramgmv@gmail.com>
2022-02-23 13:07:35 +0530

    Done

---
+++ src/com/exe/Sample.java
@@ -0,0 +1,15 @@
+package com.exe;
+
+public class Sample {
+
+	public static void main(String[] args) {
+
+		
+		
+		System.out.println("one");
+		System.out.println("two");
+		System.out.println("three");
+		System.out.println("four");
+	}
+
+}
